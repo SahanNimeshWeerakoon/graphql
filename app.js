a6383612ebc7fc9f0944ab7e2488ed5310a9f011
SahanNimeshWeerakoon <hsn.weerakoon.m@gmail.com>
2026-03-15 01:18:47 +0530

    initialize graphql

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
 const express = require('express');
+const {graphqlHTTP} = require('express-graphql');
+const { GraphQLSchema, GraphQLObjectType, GraphQLString } = require('graphql');
 
 const app = express();
+
+const schema = new GraphQLSchema({
+    query: new GraphQLObjectType({
+        name: 'Hello World',
+        fields: () => ({
+            message: {
+                type: GraphQLString,
+                resolve: () => 'Hello World'
+            }
+        })
+    })
+});
+
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: true
+}));
 
 const PORT = process.env.PORT || 5000;
 
